4c97f21169404a884306c08573f8bee6fa5ba22a
AlicinhaDavid <alicedaviddossantos@gmail.com>
2024-05-16 17:17:02 -0300

    feat: create the main aside menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-
+import { HomeIcon, Search, Library } from "lucide-react";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col"> 
      <div className="flex flex-1">
        <aside className="w-72 bg-zinc-950 p-6">
-         sidebar
+         <nav className="space-y-5">
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <HomeIcon />
+            Home
+          </a>
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <Search />
+            Search
+          </a>
+          <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <Library />
+            Your Library
+          </a>
+         </nav>
        </aside>      
        <main className="flex-1 p-6">
          main
